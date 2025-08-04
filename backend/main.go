29a78833db16bfa1f64db0e3f6c374ca8de76f0c
Nima Makhmali <nmkhmly790@gmail.com>
2025-08-04 02:24:43 -0700

    feat: add packages

---
+++ backend/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
+	"github.com/nimamakhmali/turing-machine-go/turing"
+)
+
+
+func main() {
+	r := gin.Default()  //router
+	
+
+}
