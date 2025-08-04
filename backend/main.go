f15fdf0c3bc64716fa0757b8bc6cd5d7cb797bb0
Nima Makhmali <nmkhmly790@gmail.com>
2025-08-04 02:24:43 -0700

    feat: add packages

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+<<<<<<< HEAD
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
@@ -18,3 +19,21 @@ func main() {
 	r.LoadHTMLGlob("frontend/templates/*")
 
 }
+=======
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
+	"github.com/nimamakhmali/turing-machine-go/turing"
+)
+
+
+func main() {
+	r := gin.Default()  //router
+	
+
+}
+>>>>>>> 29a7883 (feat: add packages)
