1feda4af80c995db757d5ac9bc4dadd330974ab5
Nima Makhmali <nmkhmly790@gmail.com>
2025-08-04 02:43:41 -0700

    feat: add structs and improve code organization

--- backend/main.go
+++ backend/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 <<<<<<< HEAD
+<<<<<<< HEAD
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
@@ -26,14 +27,26 @@ func main() {
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+=======
+>>>>>>> 033d287 (feat: add structs and improve code organization)
 	"github.com/gin-contrib/cors"
-	"github.com/nimamakhmali/turing-machine-go/turing"
+	"github.com/gin-gonic/gin"
 )
 
-
 func main() {
-	r := gin.Default()  //router
-	
+	r := gin.Default() //router
+	r.Use(cors.Default())
+	api := r.Group("/api")
+	{
+		api.POST("/simulate", simulateTuringMachine)
+		api.GET("/example", getExamples)
+		api.GET("/example/:name", getExample)
+	}
+	r.Static("/static", "./frontend/static")
+	r.LoadHTMLGlob("frontend/templates/*")
 
 }
+<<<<<<< HEAD
 >>>>>>> 29a7883 (feat: add packages)
+=======
+>>>>>>> 033d287 (feat: add structs and improve code organization)
